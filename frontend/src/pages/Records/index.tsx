96398010ac719806bf55dc998c362f57153c8ce7
samaslima <samarinhalimag@gmail.com>
2020-10-25 15:12:44 -0300

    Pagination

--- frontend/src/pages/Records/index.tsx
+++ frontend/src/pages/Records/index.tsx
@@ -2,16 +2,23 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './styles.css';
 import { RecordsResponse } from './types';
+import { formatDate } from './helpers';
+import Pagination from './Pagination';
 
 const BASE_URL = 'http://localhost:8080'
 
 const Records = () => {
     const [ recordsResponse, setRecordsResponse ] = useState<RecordsResponse>();
+    const [ activePage, setActivePage ] = useState(0);
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/records?linesPerPage=12`)
+        axios.get(`${BASE_URL}/records?linesPerPage=12&page=${activePage}`)
             .then(response => setRecordsResponse(response.data));
-    }, []);
+    }, [activePage]);
+
+    const handlePageChange = (index: number) => {
+        setActivePage(index)
+    } 
 
     return (
         <div className="page-container">
@@ -29,7 +36,7 @@ const Records = () => {
                 <tbody>
                     {recordsResponse?.content.map(record => (
                         <tr key={record.id}>
-                            <td>{record.moment}</td>
+                            <td>{formatDate(record.moment)}</td>
                             <td>{record.name}</td>
                             <td>{record.age}</td>
                             <td className="text-secondary">{record.gamePlatform}</td>
@@ -39,6 +46,7 @@ const Records = () => {
                     ))}
                 </tbody>
             </table>
+            <Pagination activePage={activePage} goToPage={handlePageChange} totalPages={recordsResponse?.totalPages} />
         </div>
     );
 }
